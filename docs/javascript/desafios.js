890b7221eda9e57c308ad28b81c0c135ab8d2b86
andrespeco0987 <andrespeco0987@gmail.com>
2024-03-07 20:55:57 -0500

    cambios en js

---
+++ docs/javascript/desafios.js
@@ -0,0 +1,11 @@
+"use strict"
+const entrada = prompt("Ingrese su nombre:");
+
+if(entrada)
+{
+    document.write("Hola, " + entrada + " Bienvenido!");
+}
+else
+{
+    document.write("Hola Bienvenido");
+}
