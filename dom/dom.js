ffa62600f4a97f3b6251043b8565d1cafef7c9fa
Andres Felipe Rangel Peña <andrespeco0987@gmail.com>
2024-04-17 21:44:10 -0500

    aprendisaje de dom

--- dom/dom.js
+++ dom/dom.js
@@ -1,6 +1,8 @@
 "use strict";
+//creacion de elementos
 const d = document;
 const $root = d.getElementById("root");
+const $raiz = d.getElementById("raiz");
 const $h1 = d.createElement("h1");
 const $p = d.createElement("p");
 const $btn = d.createElement("button");
@@ -9,21 +11,33 @@ const $btn3 = d.createElement("button");
 
 let contadorDeClicks = 0;
 
+// contenido de la etiqueta
 $h1.textContent = "Manipulacion Del Dom";
 $p.textContent = contadorDeClicks;
 $btn.textContent = "Mas";
 $btn2.textContent = "Menos";
 $btn3.textContent = "reset";
 
+//estilos
+$root.style.backgroundColor = "green";
+$root.style.color = "white";
+$raiz.style.backgroundColor = "greenyellow";
+
+//asociacion de hijos al padre
 $root.appendChild($h1);
 $root.appendChild($p);
 $root.appendChild($btn);
 $root.appendChild($btn2);
 $root.appendChild($btn3);
 
+//eventos
 $btn.addEventListener("click", () => {
 	contadorDeClicks++;
 	$p.textContent = contadorDeClicks;
+	if (contadorDeClicks === 10) {
+		/* $raiz.style.display = "none"; */
+		$raiz.remove(); //remover con dom
+	}
 });
 
 $btn2.addEventListener("click", () => {
@@ -34,4 +48,6 @@ $btn2.addEventListener("click", () => {
 $btn3.addEventListener("click", () => {
 	contadorDeClicks = 0;
 	$p.textContent = contadorDeClicks;
+	/* $raiz.style.display = "block"; */
+	$root.appendChild($raiz);
 });
