d921e1e491c6c99e81c9a8c14a77a73f9406c545
Andres Felipe Rangel Peña <andrespeco0987@gmail.com>
2024-05-08 18:51:26 -0500

    typescript

---
+++ typescript/script.js
@@ -0,0 +1,9 @@
+var miVariable = "Hola";
+var miNumber = 150;
+var miBoolean = true;
+var miBoolean2;
+miBoolean2 = false;
+console.log(miVariable);
+console.log(miNumber);
+console.log(miBoolean);
+console.log(miBoolean2);
---
+++ typescript/script.ts
@@ -0,0 +1,9 @@
+let miVariable = "Hola"
+let miNumber: number = 150
+let miBoolean = true
+let miBoolean2: boolean
+miBoolean2 = false
+console.log(miVariable);
+console.log(miNumber);
+console.log(miBoolean);
+console.log(miBoolean2);
